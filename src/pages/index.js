fc98c591cb77ac3d65052f559bd0644544196699
nkand84 <nityabhagi@gmail.com>
2019-02-19 14:06:21 -0800

    adding new file contact.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from "react"
+import {Link} from "gatsby"
 
 export default () => (
 <div style={{color : 'purple'}}>
+<Link to="/contact/">Contact Us</Link>
 <h1>Hello Gatsby!</h1>
 <p>How are you ? </p>
 <img src="https://source.unsplash.com/random/400x200" alt="" />
